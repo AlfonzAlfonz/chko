710393bdb858b11cc919d7aa860d28922568c749
Denis Homolík <alfonz@homolik.cz>
2024-01-02 18:55:42 +0100

    fixes the fixes

--- components/admin/forms/utils.ts
+++ components/admin/forms/utils.ts
@@ -28,7 +28,7 @@ export const upload = async <
 
     const result = await put(path + blob.name, blob, {
       access: "public",
-      addRandomSuffix: false,
+      addRandomSuffix: true,
       token: process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN,
     });
     return {
